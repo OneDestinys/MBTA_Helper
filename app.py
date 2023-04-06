c37ba87000f20a3022f2cd36a4e7952de7ed47b4
Jacob <122861394+OneDestinys@users.noreply.github.com>
2023-04-05 20:05:36 -0400

    Revert "Update app.py"
    This reverts commit f8f05f694f07f4a8617dc69fb9ab257175d6f79e.

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ def home():
 
 @app.post('/nearest_mbta')
 def nearest_station():
-    # try:
+    try:
         address = request.form.get('address')
         mbta_info = find_stop_near(address)
         stop_id = mbta_info[0]
         arrival_time =get_arrival_time(stop_id)
         print(mbta_info)
-        return render_template('mbta_station.html', mbta_info = mbta_info)
-    # except: 
-    #     return render_template('error.html')
+        return render_template('mbta_station.html', mbta_info = mbta_info, arrival_time = arrival_time)
+    except: 
+        return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-    # , arrival_time = arrival_time
+    
